8409770fb1ba3558cc0321d4fbff94d84e203428
RomanBossio <bossio.roman2004@gmail.com>
2025-06-02 15:26:20 -0300

    Finaliza pantalla de bienvenida lista y funcional

---
+++ app/src/main/java/com/example/juegosfavoritosapp/ListaJuegosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.juegosfavoritosapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaJuegosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_juegos)
+    }
+}
