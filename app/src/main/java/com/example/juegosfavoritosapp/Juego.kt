a458f0807f40aacc37735a5788cb0cb1678b6cc7
Eap9821015 <perezalexis9816@gmail.com>
2025-06-07 19:50:38 -0300

    Funcionalidades de favoritos completas

--- app/src/main/java/com/example/juegosfavoritosapp/Juego.kt
+++ app/src/main/java/com/example/juegosfavoritosapp/Juego.kt
@@ -4,5 +4,6 @@ data class Juego(
     val nombre: String,
     val descripcion: String,
     val imagenUri: String?, // URI como String. Puede ser nulo si se usa la imagen por defecto.
-    val categoria: String // Nueva categoría del juego
+    val categoria: String,  // Nueva categoría del juego
+    var esFavorito: Boolean = false // NUEVO: indica si es favorito o no
 )
